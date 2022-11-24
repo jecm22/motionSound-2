932bb443cf5aab3796bd7422b011126d786c5212
rapinona <rapinona@liverpool.com.mx>
2022-11-23 20:43:27 -0600

    Initial Commit

---
+++ motionSound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  motionSound
+//
+//  Created by Piñón Ayala Rodrigo  on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
